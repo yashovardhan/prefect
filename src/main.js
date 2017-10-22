a7212467d682d0d957b3331e96f775fd57eeddff
Joe Nash <joenash@github.com>
2017-10-22 04:04:03 +0100

    added to main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 const Greeting = require('./actions/greeting')
+const Mdlint = require('./actions/mdlint')
 const CheckIfMoreThanOneModuleIsBeingSubmitted = require('./actions/multiple-module-warning')
 
 module.exports = (robot) => {
   // Your code here
   console.log('Yay, the app was loaded!')
 
-	
+
 	robot.on('issues.opened', Greeting)
+  robot.on('pull_request_opened', Mdlint)
+  
 	// robot.on('pull_request.opened', CheckIfMoreThanOneModuleIsBeingSubmitted)
 
   // For more information on building apps:
