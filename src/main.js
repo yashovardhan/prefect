a8860d5891f66bdc1ba23bc91013bc6b52c9b5e1
Kai Barclay <adeiaxc@gmail.com>
2017-10-22 20:57:13 -0500

    preliminary copy/pasting to make pr template

--- src/main.js
+++ src/main.js
@@ -1,17 +1,60 @@
 const Greeting = require('./actions/greeting')
-const CheckIfMoreThanOneModuleIsBeingSubmitted = require('./actions/multiple-module-warning')
+const getComment = require('./actions/pr-template')
+const defaultConfig = require('./actions/pr-template')
+// const unchanged = require
 
 module.exports = (robot) => {
   // Your code here
   console.log('Yay, the app was loaded!')
 
-	
-	robot.on('issues.opened', Greeting)
-	// robot.on('pull_request.opened', CheckIfMoreThanOneModuleIsBeingSubmitted)
+  robot.on('issues.opened', Greeting)
+  robot.on(['pull_request.opened', 'issues.opened'], receive)
+  async function receive (context) {
+    let title
+    let body
+    let badTitle
+    let user
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    let eventSrc = 'issue'
+    if (context.payload.pull_request) {
+      ({title, body, user} = context.payload.pull_request)
+      eventSrc = 'pullRequest'
+    } else {
+      ({title, body, user} = context.payload.issue)
+    }
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    try {
+      const config = await context.config('config.yml', defaultConfig)
+
+      if (!config.requestInfoOn[eventSrc]) {
+        return
+      }
+
+      if (config.requestInfoDefaultTitles) {
+        if (config.requestInfoDefaultTitles.includes(title.toLowerCase())) {
+          badTitle = true
+        }
+      }
+
+      let notExcludedUser = true
+      if (config.requestInfoUserstoExclude) {
+        if (config.requestInfoUserstoExclude.includes(user.login)) {
+          notExcludedUser = false
+        }
+      }
+      if ((!body || badTitle) && notExcludedUser) {
+        const comment = getComment(config.requestInfoReplyComment, defaultConfig.requestInfoReplyComment)
+        context.github.issues.createComment(context.issue({body: comment}))
+
+        if (config.requestInfoLabelToAdd) {
+            // Add label if there is one listed in the yaml file
+          context.github.issues.addLabels(context.issue({labels: [config.requestInfoLabelToAdd]}))
+        }
+      }
+    } catch (err) {
+      if (err.code !== 404) {
+        throw err
+      }
+    }
+  }
 }
